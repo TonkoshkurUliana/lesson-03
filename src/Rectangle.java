b212f0f5e327c0faa01919c51438661b024fe81c
TonkoshkurUliana <118213848+TonkoshkurUliana@users.noreply.github.com>
2022-11-22 14:17:55 +0200

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Rectangle {
+
+public class Rectangle {
+    private double dovgina;
+    private double shirina;
+
+    Rectangle() {
+        this.dovgina = 7.6;
+        this.shirina = 15;
+    }
+
+    Rectangle(double dovgina, double shirina) {
+        this.dovgina = dovgina;
+        this.shirina = shirina;
+    }
+
+    double getRectangleArea() {
+        double area = dovgina * shirina;
+        return area;
+    }
+
+    double getRectanglePerimeter() {
+        double perimetr = 2 * (dovgina + shirina);
+        return perimetr;
+    }
 }
