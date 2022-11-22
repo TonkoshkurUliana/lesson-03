3260d682c69cc5fa9ebf3c1d884df875a37f37c8
TonkoshkurUliana <118213848+TonkoshkurUliana@users.noreply.github.com>
2022-11-22 14:17:14 +0200

    Update Kolo.java

--- src/Kolo.java
+++ src/Kolo.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Kolo {
+public class Kolo {
+    private double radius;
+    private double diametr;
+
+    Kolo (double radius,double diametr) {
+        this.radius = radius;
+        this.diametr = diametr;
+    }
+
+    double getArea() {
+        double area = (diametr * diametr) / 4 * Math.PI;
+        return area;
+    }
+    double getLenght() {
+        double lenght = 2*Math.PI*radius;
+        return lenght;
+    }
+
 }
