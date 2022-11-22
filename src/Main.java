5b5a26ffe106ea298331da23069a97c33e43cc53
TonkoshkurUliana <118213848+TonkoshkurUliana@users.noreply.github.com>
2022-11-22 14:17:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double radius;
+        double diametr;
+
+        Kolo myCircle = new Kolo(5.5, 11.0);
+        Rectangle myRectangle = new Rectangle();
+        Rectangle myRectangle2 = new Rectangle(15, 20);
+
+        System.out.println("Довжина кола: " + myCircle.getLenght());
+        System.out.println("Площа кола: " + myCircle.getArea());
+
+        System.out.println("Площа прямокутника (без параметрів): " + myRectangle.getRectangleArea());
+        System.out.println("Периметр прямокутника (без параметрів): " + myRectangle.getRectanglePerimeter());
+
+        System.out.println("Площа прямокутника (з параметрів): " + myRectangle2.getRectangleArea());
+        System.out.println("Периметр прямокутника (з параметрів): " + myRectangle2.getRectanglePerimeter());
     }
 }
